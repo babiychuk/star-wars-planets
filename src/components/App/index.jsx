9ea0ef3cb8ff842df912d093887b9ae28a7b9daa
Evgeniy <evgeniy@MacBook-Pro-Evgeniy.local>
2020-12-13 14:21:56 +0200

    add first route

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Suspense } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import routes from "../../routes";
+import Header from "../Base/Header";
 
 const App = () => {
-    return (
-        <div>
-           app
-        </div>
-    );
+  return (
+    <>
+    <Header />
+      <main>
+        <section>
+          <Suspense fallback={null}>
+            <Switch>
+              <Route
+                path={routes.Root.path}
+                component={routes.Root.component}
+              />
+
+              <Redirect to={routes.Root.path} />
+            </Switch>
+          </Suspense>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  Root: {
+    path: '/star-wars-planets',
+    component: React.lazy(() =>
+      import('../components/Pages/Planets' /* webpackChunkName: "Home" */),
+    ),
+  },
+};
+
