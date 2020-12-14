4ffd7d907ba1d6d16f9013c21ba7aa15903bb585
Evgeniy <evgeniy@MacBook-Pro-Evgeniy.local>
2020-12-14 20:50:00 +0200

    fix error, change modal

--- src/components/Base/Modal/index.jsx
+++ src/components/Base/Modal/index.jsx
@@ -2,42 +2,65 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { closeModal } from "../../../redux/modal/actions";
 
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 
+const modalStyles = makeStyles((theme) => ({
+  modal: {
+    position: "fixed",
+    display: "flex",
+    width: "100%",
+    height: "100%",
+    left: "0",
+    top: "0",
+    overflow: "auto",
+    zIndex: "199",
+    background: "rgba(0,0,0,.7)",
+  },
+  modalInner: {
+    margin: "auto",
+    position: "relative",
+    padding: "10px 0",
+    width: "100%",
+  },
+  modalContent: {
+    backgroundColor: "#fff",
+    width: "720px",
+    margin: "0 auto",
+  },
+}));
+
 const Modal = () => {
+  const classes = modalStyles();
   const dispatch = useDispatch();
 
   const DomElement = useSelector((store) => store.modal).domElement;
-  const { isModal } = useSelector((state) => state.modal);
 
   const handleClose = () => {
     dispatch(closeModal());
   };
 
   return (
-    <>
-      <Dialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={isModal}
-      >
-        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-          Resident info
-        </DialogTitle>
-        <DialogContent dividers>
-          {DomElement ? <DomElement /> : null}
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose} color="primary">
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <div className={classes.modal}>
+      <div className={classes.modalInner}>
+        <div className={classes.modalContent}>
+          <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+            Resident info
+          </DialogTitle>
+          <DialogContent dividers>
+            {DomElement ? <DomElement /> : null}
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleClose} color="primary">
+              Close
+            </Button>
+          </DialogActions>
+        </div>
+      </div>
+    </div>
   );
 };
 
